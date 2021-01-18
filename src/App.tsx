898c344ec1cc13ec474e702438258932f1d1b65a
Nguyen Van Hoa <hoahenry069@gmail.com>
2021-01-18 13:53:50 +0700

    HU-61: refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,29 @@
-import React from 'react';
-import Button from './components/button';
-import ElCol from './components/col/col';
-import Row from './components/row/row';
-import './theme/index.scss';
+import React from "react";
+import Button from "./components/button";
+import ElCol from "./components/col/col";
+import Row from "./components/row/row";
+import "./theme/index.scss";
 
 function App() {
   return (
     <div className="App">
-      <Button type="warning" round >Danger</Button>
-      <Row justify="center" aligh="middle" >
-      <ElCol span={6}  >
-        <div className="grid-content bg-purple">col-1</div>
-      </ElCol>
-      <ElCol span={6}  >
-        <div className="grid-content bg-purple">col-2</div>
-      </ElCol>
-      <ElCol span={6}  >
-        <div className="grid-content bg-purple">col-3</div>
-      </ElCol>
-      <ElCol span={6}  >
-        <div className="grid-content bg-purple">col-4</div>
-      </ElCol>
+      <Button type="warning" round>
+        Danger
+      </Button>
+      <Row justify="center" aligh="middle">
+        <ElCol span={6}>
+          <div className="grid-content bg-purple">col-1</div>
+        </ElCol>
+        <ElCol span={6}>
+          <div className="grid-content bg-purple">col-2</div>
+        </ElCol>
+        <ElCol span={6}>
+          <div className="grid-content bg-purple">col-3</div>
+        </ElCol>
+        <ElCol span={6}>
+          <div className="grid-content bg-purple">col-4</div>
+        </ElCol>
       </Row>
-      
     </div>
   );
 }
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,18 +1,18 @@
-import * as React from 'react';
-import classNames from 'classnames';
+import * as React from "react";
+import classNames from "classnames";
 
 export interface BaseButtonProps {
   type?: String;
   size?: String;
   icon?: string;
-  nativeType?: 'button' | 'submit' | 'reset'; 
-  loading?: boolean,
-  disabled?: boolean,
-  plain?: boolean,
-  autofocus?: boolean,
-  round?: boolean,
-  circle?: boolean
-  children?: string | React.ReactNode,
+  nativeType?: "button" | "submit" | "reset";
+  loading?: boolean;
+  disabled?: boolean;
+  plain?: boolean;
+  autofocus?: boolean;
+  round?: boolean;
+  circle?: boolean;
+  children?: string | React.ReactNode;
 }
 
 const Button: React.FunctionComponent<BaseButtonProps> = ({
@@ -28,52 +28,40 @@ const Button: React.FunctionComponent<BaseButtonProps> = ({
   circle,
   children,
 }: BaseButtonProps) => {
-  const classes = classNames(
-    'hive-button',
-    {
-      [`hive-button--${type}`]: type,
-      [`hive-button--${size}`]: size,
-      'is-disabled': disabled,
-      'is-loading': loading,
-      'is-plain': plain,
-      'is-round': round,
-      'is-circle': circle
-    },
-  );
+  const classes = classNames("hive-button", {
+    [`hive-button--${type}`]: type,
+    [`hive-button--${size}`]: size,
+    "is-disabled": disabled,
+    "is-loading": loading,
+    "is-plain": plain,
+    "is-round": round,
+    "is-circle": circle,
+  });
   return (
     <button
       className={classes}
-      disabled={disabled || loading }
+      disabled={disabled || loading}
       autoFocus={autofocus}
       type={nativeType}
-
     >
-      {
-        loading && <i className="hive-icon-loading"/>
-      }
-      {
-        icon && !loading && <i className={icon}></i>
-      }
-      <span>
-        {
-          children
-        }
-      </span>
+      {loading && <i className="hive-icon-loading" />}
+      {icon && !loading && <i className={icon}></i>}
+      <span>{children}</span>
     </button>
-  )
+  );
 };
-  
+
 Button.defaultProps = {
-  type: 'default',
-  icon: '',
-  nativeType: 'button',
-  size: 'medium',
+  type: "default",
+  icon: "",
+  nativeType: "button",
+  size: "medium",
   loading: false,
   disabled: false,
   plain: false,
   autofocus: false,
   round: false,
-  circle: false
-}
+  circle: false,
+};
 
 export default Button;
